dafa6d54145747fddb21097575dbd15600042201
0xpolarzero <0xpolarzero@gmail.com>
2025-01-27 16:44:11 +0100

    chore: add prettier plugin to format jsdoc

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
     },
