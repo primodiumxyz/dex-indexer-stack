5bbe4ea988ba20eb7ed6787dc48535e79a09336e
0xpolarzero <0xpolarzero@gmail.com>
2025-01-28 12:48:00 +0100

    docs(indexer): document parseEnv

--- packages/indexer/bin/parseEnv.ts
+++ packages/indexer/bin/parseEnv.ts
@@ -1,16 +1,30 @@
 import { z, ZodError, ZodIntersection, ZodTypeAny } from "zod";
 
+// Schema for environment variables passed to the root `.env` file or `docker-compose.yml` file
 const commonSchema = z.object({
+  // Node environment
+  // - "local" & "test" will use local Hasura and Timescale databases
+  // - "production" will use production Hasura instance as specified below (which should point to the production Timescale instance)
   NODE_ENV: z.enum(["local", "test", "production"]).default("local"),
-  SERVER_HOST: z.string().default("0.0.0.0"),
-  SERVER_PORT: z.coerce.number().positive().default(8888),
+  // URL of the Hasura instance (this should not include the path, e.g. "/v1/graphql")
   HASURA_URL: z.string().default("http://localhost:8090"),
+  // Admin secret to write to the Hasura instance
   HASURA_ADMIN_SECRET: z.string(),
+  // URL of the Quicknode endpoint
   QUICKNODE_ENDPOINT: z.string(),
+  // Token for the Quicknode endpoint
   QUICKNODE_TOKEN: z.string(),
+  // URL of the Jupiter API endpoint, including the token
   JUPITER_URL: z.string(),
 });
 
+/**
+ * Parse the environment variables and validate them against the provided schema. If no schema is provided, the common
+ * schema is used.
+ *
+ * @param schema - The schema to validate the environment variables against.
+ * @returns The parsed environment variables.
+ */
 export function parseEnv<TSchema extends ZodTypeAny | undefined = undefined>(
   schema?: TSchema,
 ): z.infer<TSchema extends ZodTypeAny ? ZodIntersection<typeof commonSchema, TSchema> : typeof commonSchema> {
